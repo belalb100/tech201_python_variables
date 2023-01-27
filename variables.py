7e22358cfe259b42f77d78952f1bb5cd46c0a51e
belalb100 <77366270+belalb100@users.noreply.github.com>
2023-01-27 15:56:13 +0000

    python task 1

---
+++ variables.py
@@ -0,0 +1,33 @@
+# Variables
+
+# variable_name = variable_date
+
+a = 1
+b = 2
+c = 3.5
+hi = "Hello World!"
+
+# print(type(hi))
+# # print(a)
+# print(1+2)
+# print(a + b)
+
+# the type method
+
+# print(type(a))
+#
+# # Overwriting variables
+# print(a)
+# a=4
+# print(a)
+
+# Getting User input
+
+# print("Hi what is your name?")
+# name = input()
+# print("hi")
+# print(name)
+
+print("Hello, what is your name, how old are you and what is your date of birth? ")
+user_info = input()
+print(user_info)
