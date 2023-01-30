ed8874fac2f530ba159aae00404eb4edc70a90d7
belalb100 <77366270+belalb100@users.noreply.github.com>
2023-01-30 12:52:56 +0000

    Hello

--- variables.py
+++ variables.py
@@ -34,6 +34,11 @@ print("and how old are you?")
 user_age = input()
 print("and what is you D.O.B. ?")
 user_DOB = input()
+print("What is full address?")
+address = input()
+print("Also what are some of your hobbies?")
+hobbies = input()
 
-print(user_name + " is " + user_age + " old " + "and was born the " + user_DOB)
+
+print(user_name + " is " + user_age + " old " + "and was born on the " + user_DOB + " and lives at " + address +  " and their hobbies include " + hobbies)
 
